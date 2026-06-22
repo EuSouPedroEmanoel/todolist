41b200b6805330222943dc394ab63151fa76737a
Pedro Emanoel <pedroemanoeldasilvadeoliveira@gmail.com>
2026-06-22 16:22:06 -0300

    feat: implementando schema

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from todolist.app import app
@@ -8,5 +10,4 @@ def test_root_deve_retornar_ola_mundo():
 
     response = client.get('/')
     assert response.json() == {'message': 'olá mundo'}
-    assert response.status_code ==  HTTPStatus.OK
-
+    assert response.status_code == HTTPStatus.OK
