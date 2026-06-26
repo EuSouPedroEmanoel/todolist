b018a9b7ab14a6703906d061e4c3724d80dfc168
Pedro Emanoel <pedroemanoeldasilvadeoliveira@gmail.com>
2026-06-25 22:12:17 -0300

    learn: adição de banco de dados e migração

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,5 @@
 from http import HTTPStatus
 
-import pytest
-from fastapi.testclient import TestClient
-
-from todolist.app import app
-
-
-@pytest.fixture
-def client():
-    return TestClient(app)
-
 
 def test_root_deve_retornar_ola_mundo(client):
 
